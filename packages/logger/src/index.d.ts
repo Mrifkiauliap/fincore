71e9449422adf64ab0eb217473d885504efc4a9e
Muhammad Rifki Aulia Pratama <mrifkiauliap2105@gmail.com>
2026-05-27 09:17:22 +0700

    refactor: move exclude property to root level of tsconfig.json

--- packages/logger/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import pino from "pino";
-export declare function createLogger(service: string): pino.Logger<never, boolean>;
-export type Logger = ReturnType<typeof createLogger>;
-//# sourceMappingURL=index.d.ts.map
